6a14f1d0acbf3aa8d4e6c9ad9244bb99c51ac124
skrobchik <robertkobylyansky@gmail.com>
2026-09-26 15:01:11 +0200

    create example

---
+++ examples/vector_sum/src/main.rs
@@ -0,0 +1,18 @@
+use krnl::macros::module;
+
+#[module]
+mod kernels {
+    #[cfg(not(target_arch = "spirv"))]
+    use krnl::krnl_core;
+    use krnl_core::macros::kernel;
+
+    #[kernel]
+    pub fn vector_sum(#[item] a: f32, #[item] b: &mut f32) {
+        a + b
+    }
+}
+
+
+fn main() {
+
+}
